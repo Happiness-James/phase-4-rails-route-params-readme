c01a004d38ec2fc0813f1189fdf2c1ceba4a7b21
Happiness-James <gesarehjames@gmail.com>
2023-03-14 05:21:27 +0000

    commit tree be8c05b7b9e9368747ae4a8f13972bae2f2ce109

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -5,4 +5,16 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  #Solution 1
+  # def show
+  #   show = Cheese.where("id=?", params[:id])
+  #   render json:show
+  # end  
+
+  #solution 2
+
+  def show
+    cheese = Cheese.find_by(id: params[:id])
+    render json:cheese
+  end
 end
